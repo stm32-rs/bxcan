ee784440714d9f5e05156f9afac16d4bd614555d
Sam Crow <scrow@eng.ucsd.edu>
2021-06-03 17:39:40 -0700

    Added function to enable/disable automatic retransmit

--- src/lib.rs
+++ src/lib.rs
@@ -269,6 +269,16 @@ impl<I: Instance> CanConfig<'_, I> {
         can.btr.modify(|_, w| w.silm().bit(enabled));
         self
     }
+
+    /// Enables or disables automatic retransmission of messages
+    ///
+    /// If this is enabled, the CAN peripheral will automatically try to retransmit each frame
+    /// util it can be sent. Otherwise, it will only try once to send each frame.
+    pub fn set_automatic_retransmit(&mut self, enabled: bool) -> &mut Self {
+        let can = self.registers();
+        can.mcr.modify(|_, w| w.nart().bit(!enabled));
+        self
+    }
 }
 
 impl<I: Instance> Drop for CanConfig<'_, I> {
